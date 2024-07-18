3597df476549ac5c968d83bddbe3417b71eb248f
heintzz <mhasnanr@gmail.com>
2024-07-18 08:49:00 +0700

    fix: api url

--- src/pages/daftar-pemeriksaan/index.jsx
+++ src/pages/daftar-pemeriksaan/index.jsx
@@ -95,7 +95,7 @@ const HalamanDaftarPemeriksaan = () => {
       </div>
 
       <div className="fixed bottom-20 right-6">
-        <a href={`${import.meta.env.BASE_ENDPOINT_URL}/reports?year=${year}&month=${month}`}>
+        <a href={`${import.meta.env.VITE_BASE_ENDPOINT_URL}/reports?year=${year}&month=${month}`}>
           <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full shadow-lg flex items-center">
             <RiFileExcel2Fill className="mr-2" size={20} />
             Download Excel
