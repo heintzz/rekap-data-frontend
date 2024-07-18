183b62f22ebca8554329139ae5a835b278db26db
heintzz <mhasnanr@gmail.com>
2024-07-18 08:47:17 +0700

    fix: download excel url

--- src/pages/daftar-pemeriksaan/index.jsx
+++ src/pages/daftar-pemeriksaan/index.jsx
@@ -94,17 +94,8 @@ const HalamanDaftarPemeriksaan = () => {
           })}
       </div>
 
-      {/* <div className="fixed inset-0 -translate-y-20 flex items-end justify-center">
-        <a href={`http://localhost:8080/reports?year=${year}&month=${month}`}>
-          <button className="flex items-center bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full shadow-lg">
-            <RiFileExcel2Fill className="mr-2" size={20} />
-            Download Laporan
-          </button>
-        </a>
-      </div> */}
-
       <div className="fixed bottom-20 right-6">
-        <a href={`${import.meta.env.BASE_ENDPOINT_URL}reports?year=${year}&month=${month}`}>
+        <a href={`${import.meta.env.BASE_ENDPOINT_URL}/reports?year=${year}&month=${month}`}>
           <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full shadow-lg flex items-center">
             <RiFileExcel2Fill className="mr-2" size={20} />
             Download Excel
