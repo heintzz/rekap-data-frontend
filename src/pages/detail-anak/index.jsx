b728a8dc03acfa2ae1dcd4d90674f3bcd4f9efb5
heintzz <mhasnanr@gmail.com>
2024-07-28 10:00:29 +0700

    fix: address change

--- src/pages/detail-anak/index.jsx
+++ src/pages/detail-anak/index.jsx
@@ -162,7 +162,15 @@ const HalamanDetailAnak = () => {
               <select
                 name="dusun"
                 className="w-full h-10 px-3 py-2 border border-gray-300 rounded-md focus:outline-none"
-                onChange={handleValueChange}
+                onChange={(e) => {
+                  setFormData((prev) => ({
+                    ...prev,
+                    alamat: {
+                      ...prev.alamat,
+                      dusun: e.target.value,
+                    },
+                  }));
+                }}
                 value={formData?.alamat?.dusun || ''}
               >
                 <option value="" disabled>
